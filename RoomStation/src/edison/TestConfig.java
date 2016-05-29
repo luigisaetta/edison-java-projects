555a2a605ae910b43d98fa1859ee87e801fce7ee
luigisaetta <luigi.saetta@gmail.com>
2016-05-29 11:14:16 +0200

    removed import of Assert class

---
+++ RoomStation/src/edison/TestConfig.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package edison;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author LSaetta
+ * 
+ * The idea is to use JUnit to add a suite of JUnit test
+ *
+ */
+public class TestConfig
+{
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	public void test()
+	{
+		assert(true);
+	}
+
+}
